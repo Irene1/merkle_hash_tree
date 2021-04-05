d5c570c8db7ada6ce6af08f601bb923b34f1263d
Irene L Bhaskaran <bhaskari@gvsu.edu>
2021-04-05 08:28:09 -0400

    Update merkle_tree.py

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,7 +1,11 @@
 '''
-Input: Pathnames of files.
+This program will compute Top Hash using sha1 dnd print to a file and screen
+Input:Pathnames of files to be tested for integrity(/home/bhaskari/python/my_directory/*.txt)
+OutPut: The top hash will be computed and displayed on screen/file. Top hast will not match when one or more files are modified
 There should be no upper bound on the number of files.Program should be able to handle thousands of files.
 '''
+
+# pathname of files to be tested for integrity.
 file = glob.glob("/home/bhaskari/python/my_directory/*.txt")
 
 
